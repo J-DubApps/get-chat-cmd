cf8be61a356a5e39d98bbe29f4c312049d3c9d8b
Julian West <west.julian@me.com>
2025-04-07 13:00:42 -0500

    Update ai_powershell_commands.ps1

--- ai_powershell_commands.ps1
+++ ai_powershell_commands.ps1
@@ -1,3 +1,17 @@
+<#
+                .SYNOPSIS
+                    J-Dub's get-chat-cmd Functions
+                .DESCRIPTION
+                    This PS Script provides access to my current Functions leveraging two different of my favorite 
+                    LLMs for PS command-building (via popular AI LLMs) on-the-fly.  
+                    For more info see https://github.com/J-DubApps/get-chat-cmd/blob/main/README.md
+                .NOTES
+                      NOTE: You must have an OpenAI and OpenRouter key to use both of these functions
+                    Julian West
+                    julianwest.me
+                    https://github.com/J-DubApps
+#>
+
 function get-chat-cmd1 {
     param(
         [Parameter(Mandatory = $true)]
